d2ee134c268cf8af8c179dad3f31722a53ee7be5
None <devnull@localhost>
2008-12-01 22:47:05 -0500

    [svn r102] Tagged for 1.8.0

---
+++ tags/1.8.0/src/com/evancharlton/mileage/io/output/CSVView.java
@@ -0,0 +1,106 @@
+package com.evancharlton.mileage.io.output;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Message;
+import android.widget.TextView;
+import au.com.bytecode.opencsv.CSVWriter;
+
+import com.evancharlton.mileage.FillUps;
+import com.evancharlton.mileage.FillUpsProvider;
+import com.evancharlton.mileage.Mileage;
+import com.evancharlton.mileage.R;
+
+public class CSVView extends ExportView {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState, "csv");
+		m_title = (TextView) findViewById(R.id.title);
+		m_title.setText(R.string.csv_file);
+
+		super.m_exporter = new Runnable() {
+			public void run() {
+				try {
+					CSVWriter csv = new CSVWriter(new FileWriter(Environment.getExternalStorageDirectory() + "/" + getFilename()));
+
+					HashMap<String, String> fillupsProjection = FillUpsProvider.getFillUpsProjection();
+					Set<String> tmp_keySet = fillupsProjection.keySet();
+					HashSet<String> keySet = new HashSet<String>(tmp_keySet);
+					keySet.remove(FillUps._ID);
+					String[] proj = keySet.toArray(new String[keySet.size()]);
+					SQLiteDatabase db = SQLiteDatabase.openDatabase("/data/data/" + Mileage.PACKAGE + "/databases/" + FillUpsProvider.DATABASE_NAME, null, SQLiteDatabase.OPEN_READONLY);
+					Cursor c = db.query(FillUpsProvider.FILLUPS_TABLE_NAME, proj, null, null, null, null, FillUps._ID + " ASC");
+					c.moveToFirst();
+
+					// transform the keySet into the plain english form
+					ArrayList<String> removed = new ArrayList<String>();
+					ArrayList<String> added = new ArrayList<String>();
+					for (String key : keySet) {
+						String english = FillUps.PLAINTEXT.get(key);
+						if (english != null) {
+							added.add(english);
+							removed.add(key);
+						}
+					}
+					keySet.addAll(added);
+					keySet.removeAll(removed);
+
+					csv.writeNext(keySet.toArray(new String[keySet.size()]));
+					while (c.isAfterLast() == false) {
+						String[] data = new String[keySet.size()];
+						for (int i = 0; i < c.getColumnCount(); i++) {
+							data[i] = c.getString(i);
+						}
+						csv.writeNext(data);
+						c.moveToNext();
+					}
+					c.close();
+					csv.close();
+					m_handler.post(new Runnable() {
+						public void run() {
+							Bundle data = new Bundle();
+							data.putString(MESSAGE, getString(R.string.export_finished_msg) + "\n" + getFilename());
+							data.putString(TITLE, getString(R.string.success));
+
+							Message msg = new Message();
+							msg.setData(data);
+							m_handler.handleMessage(msg);
+						}
+					});
+				} catch (final IOException e) {
+					e.printStackTrace();
+					m_handler.post(new Runnable() {
+						public void run() {
+							Bundle data = new Bundle();
+							data.putString(MESSAGE, e.getMessage());
+							data.putString(TITLE, getString(R.string.error));
+
+							Message msg = new Message();
+							msg.setData(data);
+							m_handler.handleMessage(msg);
+						}
+					});
+					return;
+				}
+			}
+		};
+	}
+
+	@Override
+	protected String getHelp() {
+		return getString(R.string.help_import_csv);
+	}
+
+	@Override
+	protected String getHelpTitle() {
+		return getString(R.string.help_import_csv_title);
+	}
+}
