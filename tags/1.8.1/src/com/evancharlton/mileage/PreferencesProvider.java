c05f65bd22e3eba26196a06d2f3056a43bd00dd2
None <devnull@localhost>
2008-12-08 19:56:21 -0500

    [svn r107] Tagged for 1.8.1

---
+++ tags/1.8.1/src/com/evancharlton/mileage/PreferencesProvider.java
@@ -0,0 +1,210 @@
+package com.evancharlton.mileage;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+
+import com.evancharlton.mileage.calculators.CalculationEngine;
+
+public class PreferencesProvider {
+	private static PreferencesProvider s_instance = null;
+	private Activity m_activity;
+	private SharedPreferences m_settings;
+	private CalculationEngine m_calcEngine;
+
+	private static final String PREFS_NAME = "MileageSettings";
+	private static final int BOOLEAN = 0;
+	private static final int FLOAT = 1;
+	private static final int INT = 2;
+	private static final int LONG = 3;
+	private static final int STRING = 4;
+
+	public static final int GALLONS = 0;
+	public static final int LITRES = 1;
+	public static final int IMP_GALLONS = 2;
+
+	public static final int MILES = 0;
+	public static final int KILOMETERS = 1;
+	public static final int KILOMETERS_100 = -1;
+
+	public static final int MI_PER_GALLON = 0;
+	public static final int KM_PER_GALLON = 1;
+	public static final int MI_PER_IMP_GALLON = 2;
+	public static final int KM_PER_IMP_GALLON = 3;
+	public static final int MI_PER_LITRE = 4;
+	public static final int KM_PER_LITRE = 5;
+	public static final int GALLONS_PER_CKM = 6;
+	public static final int LITRES_PER_CKM = 7;
+	public static final int IMP_GALLONS_PER_CKM = 8;
+
+	private PreferencesProvider(Activity activity) {
+		m_activity = activity;
+		m_settings = m_activity.getSharedPreferences(PREFS_NAME, 0);
+	}
+
+	public static PreferencesProvider getInstance(Activity activity) {
+		if (s_instance == null) {
+			s_instance = new PreferencesProvider(activity);
+		}
+		return s_instance;
+	}
+
+	public boolean write(String key, boolean value) {
+		return write(key, value, BOOLEAN);
+	}
+
+	public boolean write(String key, float value) {
+		return write(key, value, FLOAT);
+	}
+
+	public boolean write(String key, int value) {
+		return write(key, value, INT);
+	}
+
+	public boolean write(String key, long value) {
+		return write(key, value, LONG);
+	}
+
+	public boolean write(String key, String value) {
+		return write(key, value, STRING);
+	}
+
+	private boolean write(String key, Object value, int type) {
+		SharedPreferences.Editor editor = m_settings.edit();
+		switch (type) {
+			case BOOLEAN:
+				editor.putBoolean(key, (Boolean) value);
+				break;
+			case FLOAT:
+				editor.putFloat(key, (Float) value);
+				break;
+			case INT:
+				editor.putInt(key, (Integer) value);
+				break;
+			case LONG:
+				editor.putLong(key, (Long) value);
+				break;
+			case STRING:
+				editor.putString(key, (String) value);
+				break;
+			default:
+				throw new IllegalArgumentException("Unrecognized type: " + value.toString());
+		}
+		return editor.commit();
+	}
+
+	public boolean getBoolean(String key, boolean defValue) {
+		return m_settings.getBoolean(key, defValue);
+	}
+
+	public float getFloat(String key, float defValue) {
+		return m_settings.getFloat(key, defValue);
+	}
+
+	public int getInt(String key, int defValue) {
+		return m_settings.getInt(key, defValue);
+	}
+
+	public long getLong(String key, long defValue) {
+		return m_settings.getLong(key, defValue);
+	}
+
+	public String getString(String key, String defValue) {
+		return m_settings.getString(key, defValue);
+	}
+
+	public String getString(int array, String key) {
+		try {
+			int index = getInt(key, 0);
+			String data = m_activity.getResources().getStringArray(array)[index];
+			return data;
+		} catch (IndexOutOfBoundsException e) {
+			return "";
+		}
+	}
+
+	public String getCurrency() {
+		return getString(R.array.currencies, SettingsView.CURRENCY);
+	}
+
+	public CalculationEngine getCalculator() {
+		int volume = getInt(SettingsView.VOLUME, 0);
+		int distance = getInt(SettingsView.DISTANCE, 0);
+		int economy = getInt(SettingsView.ECONOMY, 0);
+		m_calcEngine = getCalculator(volume, distance, economy);
+		return m_calcEngine;
+	}
+
+	public CalculationEngine getCalculator(int volume, int distance, int economy) {
+		if (m_calcEngine == null) {
+			m_calcEngine = new CalculationEngine();
+		}
+		m_calcEngine.setInputVolume(volume);
+		m_calcEngine.setInputDistance(distance);
+		m_calcEngine.setEconomy(economy);
+		switch (economy) {
+			case MI_PER_GALLON:
+				m_calcEngine.setOutputVolume(GALLONS);
+				m_calcEngine.setOutputDistance(MILES);
+				break;
+			case KM_PER_GALLON:
+				m_calcEngine.setOutputVolume(GALLONS);
+				m_calcEngine.setOutputDistance(KILOMETERS);
+				break;
+			case MI_PER_LITRE:
+				m_calcEngine.setOutputVolume(LITRES);
+				m_calcEngine.setOutputDistance(MILES);
+				break;
+			case KM_PER_LITRE:
+				m_calcEngine.setOutputVolume(LITRES);
+				m_calcEngine.setOutputDistance(KILOMETERS);
+				break;
+			case MI_PER_IMP_GALLON:
+				m_calcEngine.setOutputVolume(IMP_GALLONS);
+				m_calcEngine.setOutputDistance(MILES);
+				break;
+			case KM_PER_IMP_GALLON:
+				m_calcEngine.setOutputVolume(IMP_GALLONS);
+				m_calcEngine.setOutputDistance(KILOMETERS);
+				break;
+			case LITRES_PER_CKM:
+				m_calcEngine.setOutputVolume(LITRES);
+				m_calcEngine.setOutputDistance(KILOMETERS_100);
+				break;
+			case GALLONS_PER_CKM:
+				m_calcEngine.setOutputVolume(GALLONS);
+				m_calcEngine.setOutputDistance(KILOMETERS_100);
+				break;
+			case IMP_GALLONS_PER_CKM:
+				m_calcEngine.setOutputVolume(IMP_GALLONS);
+				m_calcEngine.setOutputDistance(KILOMETERS_100);
+				break;
+		}
+		return m_calcEngine;
+	}
+
+	public String format(double number) {
+		// TODO: support this in the future
+		DecimalFormat formatter = new DecimalFormat("#,##0.00");
+		return formatter.format(number);
+	}
+
+	public String format(Date d) {
+		SimpleDateFormat format = new SimpleDateFormat();
+		format.applyPattern(getString(R.array.date_patterns, SettingsView.DATE));
+		return format.format(d);
+	}
+
+	public int getOrientation() {
+		int orientation = getInt(SettingsView.ORIENTATION, 0);
+		switch (orientation) {
+			case 1:
+				return ActivityInfo.SCREEN_ORIENTATION_USER;
+		}
+		return ActivityInfo.SCREEN_ORIENTATION_SENSOR;
+	}
+}
---
+++ tags/1.8.1/src/com/evancharlton/mileage/io/output/ExportView.java
@@ -0,0 +1,131 @@
+package com.evancharlton.mileage.io.output;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.evancharlton.mileage.R;
+
+public abstract class ExportView extends Activity {
+	protected final static String MESSAGE = "msg";
+	protected final static String TITLE = "title";
+
+	protected TextView m_title;
+	protected Button m_startBtn;
+	protected EditText m_filename;
+	protected String m_ext = "";
+	protected ProgressDialog m_progress = null;
+
+	public void onCreate(Bundle savedInstanceState, String ext) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.export);
+		m_ext = ext;
+	}
+
+	public void onResume() {
+		super.onResume();
+		initUI();
+	}
+
+	protected abstract String getHelpTitle();
+
+	protected abstract String getHelp();
+
+	protected void initUI() {
+		m_title = (TextView) findViewById(R.id.title);
+		m_startBtn = (Button) findViewById(R.id.start);
+		m_filename = (EditText) findViewById(R.id.filename);
+		m_filename.setText(findFilename());
+
+		m_startBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				export();
+			}
+		});
+	}
+
+	protected String findFilename() {
+		File directory = Environment.getExternalStorageDirectory();
+		String[] files = directory.list(m_filter);
+
+		String name;
+		int n = 0;
+		while (true) {
+			// search for the file
+			name = buildName(n);
+			boolean found = false;
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].equalsIgnoreCase(name)) {
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				break;
+			}
+			n++;
+		}
+		return name;
+	}
+
+	protected String getFilename() {
+		return m_filename.getText().toString().trim();
+	}
+
+	protected String buildName(int num) {
+		return "mileage" + (num > 0 ? "." + String.valueOf(num) : "") + "." + m_ext;
+	}
+
+	protected void export() {
+		m_progress = ProgressDialog.show(this, getString(R.string.exporting_title), getString(R.string.exporting));
+		Thread t = new Thread(m_exporter);
+		t.start();
+	}
+
+	protected Runnable m_exporter = null;
+
+	protected FilenameFilter m_filter = new FilenameFilter() {
+		public boolean accept(File dir, String filename) {
+			// get the extension
+			int dot_loc = filename.lastIndexOf(".");
+			if (dot_loc >= 0) {
+				String ext = filename.substring(dot_loc + 1);
+				return ext.equalsIgnoreCase(m_ext);
+			}
+			return false;
+		}
+	};
+
+	protected Handler m_handler = new Handler() {
+		public void handleMessage(Message msg) {
+			if (m_progress != null) {
+				m_progress.dismiss();
+			}
+
+			Bundle data = msg.getData();
+
+			AlertDialog dlg = new AlertDialog.Builder(ExportView.this).create();
+			dlg.setTitle(data.getString(TITLE));
+			dlg.setMessage(data.getString(MESSAGE));
+			dlg.setButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+					findFilename();
+				}
+			});
+			dlg.show();
+		}
+	};
+}
