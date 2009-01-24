860a500cb2021e8284b1a8313282c8c629833e11
evan <devnull@localhost>
2009-01-23 20:19:36 -0500

    [svn r124] Initial revision of the Instrumentation work

---
+++ trunk/tests/src/com/evancharlton/mileage/tests/MileageTest.java
@@ -0,0 +1,13 @@
+package com.evancharlton.mileage.tests;
+
+import android.test.ActivityInstrumentationTestCase;
+
+import com.evancharlton.mileage.Mileage;
+
+public class MileageTest extends ActivityInstrumentationTestCase<Mileage> {
+
+	public MileageTest() {
+		super("com.evancharlton.mileage", Mileage.class);
+	}
+
+}
