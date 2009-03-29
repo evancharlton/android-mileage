5e9f6a6fb2b6ed20d9184fd585ec4ee208aa2c25
evan <devnull@localhost>
2009-03-29 14:46:53 -0400

    [svn r156] Fixed a minor bug with calculating average fuel economy

--- trunk/src/com/evancharlton/mileage/StatisticsView.java
+++ trunk/src/com/evancharlton/mileage/StatisticsView.java
@@ -279,7 +279,7 @@ public class StatisticsView extends TabChildActivity {
 
 		double total_distance = fillups.get(fillups.size() - 1).getOdometer() - fillups.get(0).getOdometer();
 		double total_fuel = 0D;
-		for (int i = 0; i < fillups.size() - 1; i++) {
+		for (int i = fillups.size() - 1; i > 0; i--) {
 			total_fuel += fillups.get(i).getAmount();
 		}
 
