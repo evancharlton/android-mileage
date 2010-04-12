eb8618d2598ece12f1e0e16d54911385f2c8b5f7
evan@evan-desktop <evan@evan-desktop>
2010-04-11 23:12:59 -0700

    Removed the ripped out classes.

--- trunk/src/com/evancharlton/mileage/Persistent.java
+++
@@ -1,9 +0,0 @@
-package com.evancharlton.mileage;
-
-import android.os.Bundle;
-
-public interface Persistent {
-	public void saveState(Bundle outState);
-
-	public void restoreState(Bundle inState);
-}
