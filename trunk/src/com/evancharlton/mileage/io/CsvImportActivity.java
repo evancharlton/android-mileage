09a693fcb3e4c03ff6dfaf4299668a8a31ee35d2
Evan Charlton <evan@evancharlton.com>
2010-06-29 22:20:26 -0700

    Populate the UI so the user can set their field mappings.

--- trunk/src/com/evancharlton/mileage/io/CsvImportActivity.java
+++ trunk/src/com/evancharlton/mileage/io/CsvImportActivity.java
@@ -5,18 +5,24 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import au.com.bytecode.opencsv.CSVReader;
 
 import com.evancharlton.mileage.ImportActivity;
 import com.evancharlton.mileage.R;
 import com.evancharlton.mileage.provider.Settings;
+import com.evancharlton.mileage.provider.tables.FillupsTable;
 
 public class CsvImportActivity extends Activity {
 	private ColumnReaderTask mColumnReaderTask;
@@ -46,11 +52,18 @@ public class CsvImportActivity extends Activity {
 
 	public void setColumns(String[] columnNames) {
 		LayoutInflater inflater = LayoutInflater.from(this);
-		for (String columnName : columnNames) {
+		final int length = columnNames.length;
+		for (int i = 0; i < length; i++) {
+			String columnName = columnNames[i];
 			Log.d("CsvImportActivity", "Adding a UI mapping for " + columnName);
 			View v = inflater.inflate(R.layout.import_csv_mapping, mMappingContainer);
 			TextView title = (TextView) v.findViewById(R.id.title);
 			title.setText(columnName);
+			title.setId(columnName.hashCode());
+			Spinner spinner = (Spinner) v.findViewById(R.id.mappings);
+			spinner.setAdapter(new FieldAdapter(this));
+			spinner.setId(columnName.hashCode());
+			spinner.setSelection(i);
 		}
 	}
 
@@ -81,6 +94,106 @@ public class CsvImportActivity extends Activity {
 		@Override
 		protected void onPostExecute(String[] columnNames) {
 			mActivity.setColumns(columnNames);
+		}
+	}
+
+	private static class FieldAdapter implements SpinnerAdapter {
+		private final LayoutInflater mInflater;
+
+		public FieldAdapter(Context context) {
+			mInflater = LayoutInflater.from(context);
+			mContext = context;
+		}
+
+		@Override
+		public View getDropDownView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = mInflater.inflate(android.R.layout.simple_dropdown_item_1line, parent, false);
+			}
+
+			Holder holder = (Holder) convertView.getTag();
+			if (holder == null) {
+				holder = new Holder((TextView) convertView.findViewById(android.R.id.text1));
+				convertView.setTag(holder);
+			}
+			holder.text.setText(getText(position));
+
+			return convertView;
+		}
+
+		@Override
+		public int getCount() {
+			return FillupsTable.PROJECTION.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return FillupsTable.PROJECTION[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return FillupsTable.PROJECTION[position].hashCode();
+		}
+
+		private int getText(int position) {
+			return FillupsTable.PLAINTEXT[position];
+		}
+
+		@Override
+		public int getItemViewType(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = mInflater.inflate(android.R.layout.simple_spinner_item, parent, false);
+			}
+
+			TextView tv = (TextView) convertView.getTag();
+			if (tv == null) {
+				tv = (TextView) convertView.findViewById(android.R.id.text1);
+				convertView.setTag(tv);
+			}
+			tv.setText(getText(position));
+
+			return convertView;
+		}
+
+		@Override
+		public int getViewTypeCount() {
+			return 1;
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			return false;
+		}
+
+		@Override
+		public void registerDataSetObserver(DataSetObserver observer) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void unregisterDataSetObserver(DataSetObserver observer) {
+			// TODO Auto-generated method stub
+
+		}
+
+		private static class Holder {
+			public final TextView text;
+
+			public Holder(TextView text) {
+				this.text = text;
+			}
 		}
 	}
 }
