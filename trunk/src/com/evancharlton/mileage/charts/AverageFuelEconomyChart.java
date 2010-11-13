6554a38fb5272fe2d3ae91eda754003df2e151dc
Evan Charlton <evan@evancharlton.com>
2010-11-13 17:25:45 -0500

    Don't show partial fillups on the chart

--- trunk/src/com/evancharlton/mileage/charts/AverageFuelEconomyChart.java
+++ trunk/src/com/evancharlton/mileage/charts/AverageFuelEconomyChart.java
@@ -14,15 +14,17 @@ public class AverageFuelEconomyChart extends FuelEconomyChart {
 	@Override
 	protected void processCursor(LineChartGenerator generator, Cursor cursor, Vehicle vehicle) {
 		int num = 0;
-		while (cursor.isAfterLast() == false) {
+		while (cursor.moveToNext()) {
 			if (generator.isCancelled()) {
 				break;
 			}
 			if (num > 0) {
-				addPoint(cursor.getLong(0), cursor.getDouble(1));
+				double economy = cursor.getDouble(1);
+				if (economy > 0) {
+					addPoint(cursor.getLong(0), economy);
+				}
 			}
 			generator.update(num++);
-			cursor.moveToNext();
 		}
 	}
 }
