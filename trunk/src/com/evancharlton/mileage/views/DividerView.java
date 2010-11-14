59172e3efe82837b6700c49afb22762918631516
Evan Charlton <evan@evancharlton.com>
2010-11-14 12:48:58 -0500

    Made a reusable DividerView component Added explanatory dividers in the edit vehicle form.

---
+++ trunk/src/com/evancharlton/mileage/views/DividerView.java
@@ -0,0 +1,25 @@
+package com.evancharlton.mileage.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class DividerView extends TextView {
+
+	public DividerView(Context context) {
+		super(context, null, 0);
+	}
+
+	public DividerView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public DividerView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, android.R.attr.listSeparatorTextViewStyle);
+		setGravity(Gravity.CENTER_HORIZONTAL);
+		int color = context.getResources().getColor(android.R.color.primary_text_dark);
+		setTextColor(color);
+		setId(android.R.id.text1);
+	}
+}
